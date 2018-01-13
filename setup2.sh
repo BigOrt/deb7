181f0ea59d17b5af1acee20766b5c35f1f2574d3
Big_ort <adytamasurya@gmail.com>
2018-01-14 04:42:15 +0700

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,9 +1,10 @@
 echo "create vpnserver service ..........."
 echo "............................................"
+
 cd
+cd /usr/local/vpnserver/
 chmod 755 /etc/init.d/vpnserver
 mkdir /var/lock/subsys
 update-rc.d vpnserver defaults
 /etc/init.d/vpnserver start
-cd /usr/local/vpnserver/
 ./vpncmd
