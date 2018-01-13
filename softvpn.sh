c9fd3d19fc28e70ff2a4abc158a54adcb3684f34
Big_ort <adytamasurya@gmail.com>
2018-01-13 18:01:52 +0700

    Update softvpn.sh

--- softvpn.sh
+++ softvpn.sh
@@ -3,6 +3,7 @@ echo "downloading..... softethervpn"
 echo "......................................"
 echo "........................................."
 echo "..............................................."
+cd
 wget http://www.softether-download.com/files/softether/v4.24-9652-beta-2017.12.21-tree/Linux/SoftEther_VPN_Server/32bit_-_Intel_x86/softether-vpnserver-v4.24-9652-beta-2017.12.21-linux-x86-32bit.tar.gz
 tar zxvf softether-vpnserver-v4.24-9652-beta-2017.12.21-linux-x86-32bit.tar.gz
 cd vpnserver
